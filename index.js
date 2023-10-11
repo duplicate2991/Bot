0a1c04f3cd2b5fc8ae3349cf7195fd2460dde30a
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-10-11 11:54:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("hii");
 var app = require("express")();
 app.listen(3000);
 let uptime = 0;
-setIntervalsetInterval(() => {
+setInterval(() => {
   uptime++;
 }, 1000);
 app.get("/", (q, res) => {
