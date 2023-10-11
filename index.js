3653b38ed0599669fd927cebe13043d0cf841d6a
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-10-11 09:37:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("hii");
+var app = require("express")();
+app.listen(3000);
+let uptime = 0;
+setInterval(() => {
+  uptime++;
+}, 1000);
+app.get("/", (q, res) => {
+  res.send(`${uptime}`);
+});
