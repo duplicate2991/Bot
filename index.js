8a4f760225811c8c034b80ec7d78912f19e69751
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-10-11 20:20:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,4 +51,9 @@ client.on("ready", ()=> {
 });
 client.on("messageCreate", (message) => {
   if(message.bot) return;
+  if(message.content == "!ping!"){
+    message.reply({content: `${client.ws.ping}`});
+  }else if(message.content == "!uptime!"){
+        message.reply({content: `${client.uptime()}`});
+  }
 });
